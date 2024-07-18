bef79fb823a8bedf2be2a845c5d19637a8f20e6f
aryanchy4499 <158821779+aryanchy4499@users.noreply.github.com>
2024-07-18 12:51:40 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -74,7 +74,7 @@ async def token_callbackverify(_, query):
     kb.insert(0, [InlineKeyboardButton(BotTheme('ACTIVATED'), callback_data='pass activated')])
     await editReplyMarkup(query.message, InlineKeyboardMarkup(kb))
 @new_task 
-async def unverify(client,message)
+async def unverify(client,message):
     replyhi = message.reply_to_message
     user_id = replyhi.from_user.id
     data = user_data.get(user_id, {})
