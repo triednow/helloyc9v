814be7a63c456e57fe512e06e9732427bf71eecb
aryanchy4499 <158821779+aryanchy4499@users.noreply.github.com>
2024-07-23 13:43:59 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -158,10 +158,10 @@ def get_progress_bar_string(pct):
     p = min(max(pct, 0), 100)
     cFull = int(p // 8)
     cPart = int(p % 8 - 1)
-    p_str = '⚡' * cFull
+    p_str = '💠' * cFull
     if cPart >= 0:
         p_str += ['☁️', '🌤️', '⛅', '🌨️', '🌧️', '🌩️', '⛈️'][cPart]
-    p_str += '💧' * (12 - cFull)
+    p_str += '☑️' * (12 - cFull)
     return f"[{p_str}]"
 
 
